457b5afc2eeb585a134746fac3e6f95df389e98a
OleksandrHelevanNS <oleksandr.helevan@nerdysoft.com>
2025-11-26 16:41:54 +0200

    Add animations in nav

--- js/animations.js
+++ js/animations.js
@@ -19,3 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     observer.observe(title);
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const indicator = document.querySelector('.nav__indicator');
+    const links = document.querySelectorAll('.nav__link');
+
+    function updateIndicator(link) {
+        const rect = link.getBoundingClientRect();
+        const parentRect = link.closest('.nav__list').getBoundingClientRect();
+        indicator.style.left = `${rect.left - parentRect.left + rect.width / 2 - 3}px`; // центр крапки
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            links.forEach(l => l.classList.remove('nav__link--active'));
+            e.target.classList.add('nav__link--active');
+            updateIndicator(e.target);
+        });
+    });
+
+    const activeLink = document.querySelector('.nav__link--active');
+    if (activeLink) updateIndicator(activeLink);
+});
